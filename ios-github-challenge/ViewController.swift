9b0c382e9ea4c5e51e0a32738cc758866c354622
José Eduardo Vieira Morango <edvmorango@gmail.com>
2017-03-03 22:27:32 -0300

    Initial Commit

---
+++ ios-github-challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-github-challenge
+//
+//  Created by José Eduardo Vieira Morango on 3/03/17.
+//  Copyright © 2017 José Eduardo Vieira Morango. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
